c4c695930baae0ecc6365776dd857d2d86897592
yashchauhan0685-lgtm <yashchauhan0685@gmail.com>
2025-10-31 15:34:04 +0530

    Create resume.c

---
+++ resume.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    printf("Zeel Akbari\n");
+    printf("Education: First-Year Information Technology Student at DDU\n");
+    printf("Native place: Junagadh, Gujarat\n");
+    printf("Email id: akbarizeel@gmail.com | contact no.: 6351555232\n");
+    printf("\n");
+    printf("## Objective\n");
+    printf("A highly motivated first-year IT student at DDU College with a passion for software development and coding, eager to contribute to innovative projects while continuously learning.\n");
+    printf("\n");
+    printf("## Education\n");
+    printf("Dharmsinh Desai University (DDU College)\n");
+    printf("B.Tech in Information Technology\n");
+    printf("2025 - Present\n");
+    printf("Junagadh, Gujarat\n");
+    printf("\n");
+    printf("## Skills\n");
+    printf("* Programming-C (basic programming)\n");
+    printf("* HTML, CSS, Coder\n");
+    printf("* Interests: Web Development, App Development, Open source\n");
+    printf("\n");
+    printf("## Projects\n");
+    printf("(Coming soon)\n");
+    printf("Learning and building small projects to strengthen foundational skills in programming and development.\n");
+    printf("\n");
+    printf("## Hobbies & Interests\n");
+    printf("* Software Development\n");
+    printf("* Coding Challenges\n");
+    printf("* Exploring new technologies\n");
+    printf("\n");
+    printf("## Contact\n");
+    printf("Email: [akbarizeel2008@gmail.com]\n");
+    printf("Native place: Junagadh, Gujarat\n");
+    printf("Phone: 6351555232\n");
+
+    return 0;
+}
